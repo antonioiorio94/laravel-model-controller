a2652d8f6f1e930794c68d7e40f79aad360e0209
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-20 16:26:28 +0200

    visualizzazione movies tramite compact

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,6 +12,6 @@ class PageController extends Controller
     public function index(){
 
         $movies = Movie::all();
-        return view('home');
+        return view('home', compact('movies'));
     }
 }
