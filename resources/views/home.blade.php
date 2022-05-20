0c4aa59f7dfa1bec2605ea959b64c91f1e28498b
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-20 16:39:35 +0200

    aggiunta scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
-
+        <h1>Movies DB</h1>
     <div class="container">
         @if (count($movies)>0)
             @foreach ($movies as $movie)
 
-                <p>{{$movie['title']}}</p>
+                <div class="card">
+                    <h2>Title: {{$movie['title']}}</h2>
+                <p>Original Title: {{$movie['original_title']}}</p>
+                <p>Date of relase: {{$movie['date']}}</p>
+                </div>
+
+
 
                 @endforeach
 
