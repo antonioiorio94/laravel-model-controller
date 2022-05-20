5175eabc12e749f8fd7d2d89973788849f4b7894
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-20 16:32:32 +0200

    aggiunta foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,26 @@
-Ciao {{$movies}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        @if (count($movies)>0)
+            @foreach ($movies as $movie)
+
+                <p>{{$movie['title']}}</p>
+
+                @endforeach
+
+        @endif
+
+    </div>
+
+
+
+</body>
+</html>
